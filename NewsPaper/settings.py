126c741db2ec5ec972e6f7f64996794bee054898
Egor0603 <hghgegor@mail.ru>
2022-06-18 22:33:24 +0400

    cache

--- NewsPaper/settings.py
+++ NewsPaper/settings.py
@@ -59,6 +59,13 @@ INSTALLED_APPS = [
 
 ]
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+        'LOCATION': os.path.join(BASE_DIR, 'cache_files'), # Указываем, куда будем сохранять кэшируемые файлы! Не забываем создать папку cache_files внутри папки с manage.py!
+    }
+}
+
 SITE_ID = 1
 
 MIDDLEWARE = [
--- news/urls.py
+++ news/urls.py
@@ -1,18 +1,19 @@
 from django.urls import path
 from .views import PostsList, PostDetailView, NewsList, PostUpdateView, PostDeleteView, PostCreateView, IndexView, \
     subscribe
+from django.views.decorators.cache import cache_page
 
 urlpatterns = [
     path('posts/', PostsList.as_view()),
     path('news/', NewsList.as_view()),
 
-    path('search/', PostsList.as_view()),
+    path('search/', cache_page(300)(PostsList.as_view())),
     path('<int:pk>/edit', PostUpdateView.as_view(), name='post_update'),
     path('<int:pk>/delete', PostDeleteView.as_view(), name='post_delete'),
     path('<int:pk>/', PostDetailView.as_view(), name='post_detail'),
     path('add/', PostCreateView.as_view(), name='post_create'),
 
-    path('index/', IndexView.as_view(), name='index'),
+    path('index/', cache_page(60)(IndexView.as_view()), name='index'),
 
     path('subscribe/<int:pk>', subscribe, name='subscribe'),
 ]
